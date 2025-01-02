3dbbd52555ea0f52bae06496cab2dd0cd9e96b00
José Lourenço <josern1771@gmail.com>
2025-01-02 15:10:57 -0300

    Casting

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,10 +3,10 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args){
 
-        int inteiro = 10;
-        long numeroGrande = 1000000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        int inteiro = (int)100000000000L;
+        long numeroGrande = (long) 1000000.23;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float) 2500F;
         byte idadeByte = 40;
         short idadeShort = 10;
         boolean verdadeiro = true;
@@ -17,6 +17,8 @@ public class Aula02TiposPrimitivos {
         System.out.println(inteiro );
         System.out.println(falso);
         System.out.println(verdadeiro);
+
+
 
     }
 }
