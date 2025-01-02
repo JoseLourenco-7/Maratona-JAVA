ff37fe6810c77ddf4fd97422f812b6909de59434
José Lourenço <josern1771@gmail.com>
2025-01-02 14:37:38 -0300

    Tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01.java
@@ -1,4 +1,6 @@
-public class Main {
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01 {
     public static void main(String[] args){
         System.out.println("Treinamento Maratona Java");
     }
