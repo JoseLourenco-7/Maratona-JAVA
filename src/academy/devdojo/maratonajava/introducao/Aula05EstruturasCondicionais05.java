27ce92d0f0f058c9a86659ef34dd1a4c68326a5f
Jose Lourenco <josern1771@gmail.com>
2025-01-16 20:30:04 -0300

    Swith

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -1,7 +1,19 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais06 {
+public class Aula05EstruturasCondicionais05 {
     public static void main(String[] args) {
-       
+       byte dia = 1;
+       String diaSemana;
+       switch (dia){
+           case 1, 7:
+               System.out.println("Final de Semana");
+               break;
+           case 2, 3, 4, 5, 6:
+               System.out.println("Dia últil.");
+               break;
+           default:
+               System.out.println("Dia inválido");
+       }
+
     }
 }
