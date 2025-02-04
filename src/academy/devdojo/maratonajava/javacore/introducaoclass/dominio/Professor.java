2f895debef5eb3ebd2fb414762b518120110f42a
Jose Lourenco <josern1771@gmail.com>
2025-02-04 19:46:16 -0300

    Coesão

--- src/academy/devdojo/maratonajava/javacore/introducaoclass/dominio/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/introducaoclass/dominio/Professor.java
@@ -1,4 +1,7 @@
 package academy.devdojo.maratonajava.javacore.introducaoclass.dominio;
 
 public class Professor {
+    public String nome;
+    public int idade;
+    public char sexo;
 }
--- src/academy/devdojo/maratonajava/javacore/introducaoclass/teste/ProfessorTest01.java
+++ src/academy/devdojo/maratonajava/javacore/introducaoclass/teste/ProfessorTest01.java
@@ -1,4 +1,14 @@
 package academy.devdojo.maratonajava.javacore.introducaoclass.teste;
 
-public class ProfessorTest {
+import academy.devdojo.maratonajava.javacore.introducaoclass.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args ){
+        Professor professor = new Professor();
+        professor.nome = "Kenshin";
+        professor.idade = 28;
+        professor.sexo = 'M';
+
+        System.out.println("Nome: " + professor.nome + "\nIdade: " + professor.idade + "\nSexo: " + professor.sexo);
+    }
 }
